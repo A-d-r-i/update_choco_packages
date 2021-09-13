637fa19d7ddbf1669b3682fb0c34140786f7e459
A-d-r-i <a-d-r-i@outlook.fr>
2021-09-13 09:39:24 +0200

    Add files via upload

---
+++ tutanota/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Join-Path $toolsDir 'tutanota-desktop-win.exe'
+
+
+$packageArgs = @{
+  packageName   = 'tutanota'
+  unzipLocation = $toolsDir
+  file           = $fileLocation
+  fileType      = 'EXE'
+  silentArgs     = '/S'
+  softwareName  = 'tutanota*'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyInstallPackage @packageArgs
+Remove-Item $toolsDir\*.exe -ea 0 -force
