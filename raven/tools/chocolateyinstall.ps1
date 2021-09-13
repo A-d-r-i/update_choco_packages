a261fed79d00c3b83f1a47cc1792dde93fb23a16
A-d-r-i <27277698+A-d-r-i@users.noreply.github.com>
2021-09-13 07:49:49 +0000

    [Bot] Update files - Raven

--- raven/tools/chocolateyinstall.ps1
+++ raven/tools/chocolateyinstall.ps1
@@ -1,9 +1,8 @@
-﻿$packageName = 'raven'
+$packageName = 'raven'
 $installerType = 'EXE'
-$url = 'https://github.com/hello-efficiency-inc/raven-reader/releases/download/v1.0.65/Raven-Reader-Setup-1.0.65.exe'
-$checksum = '0DD0397EAE51140D07A8CA3E4FDB7B0A1F1599F835CCFE1EADDAC76D82BCC638'
+$url = 'https://github.com/hello-efficiency-inc/raven-reader/releases/download/v1.0.66/Raven-Reader-Setup-1.0.66.exe'
+$checksum = '2ABE0E179AD2D66D52566FD6F82C8C6FF58E5ED2B360B9CEB30655E02806B13E'
 $checkumType = 'sha256'
-
 $silentArgs = '/S'
 $validExitCodes = @(0)
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -checksum $checksum -checksumType $checkumType -validExitCodes $validExitCodes
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -checksum $checksum -checksumType $checkumType -validExitCodes $validExitCodes 
