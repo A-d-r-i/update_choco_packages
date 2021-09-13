48ee151ae7dd439b04afc158bd3234b3b86419e4
A-d-r-i <a-d-r-i@outlook.fr>
2021-09-13 09:41:34 +0200

    Add files via upload

---
+++ audiomoth-flash/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Join-Path $toolsDir 'AudioMothFlashAppSetup.exe'
+
+
+$packageArgs = @{
+  packageName   = 'audiomoth-flash'
+  unzipLocation = $toolsDir
+  file           = $fileLocation
+  fileType      = 'EXE'
+  silentArgs     = '/S'
+  softwareName  = 'audiomoth-flash*'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyInstallPackage @packageArgs
+Remove-Item $toolsDir\*.exe -ea 0 -force
---
+++ mendeley-reference-manager/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Join-Path $toolsDir 'mendeley-reference-manager.exe'
+
+
+$packageArgs = @{
+  packageName   = 'mendeley-reference-manager'
+  unzipLocation = $toolsDir
+  file           = $fileLocation
+  fileType      = 'EXE'
+  silentArgs     = '/S'
+  softwareName  = 'mendeley-reference-manager*'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyInstallPackage @packageArgs
+Remove-Item $toolsDir\*.exe -ea 0 -force
