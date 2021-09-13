63a53579d9595d9201f7a5a6747690a19e78be33
A-d-r-i <27277698+A-d-r-i@users.noreply.github.com>
2021-09-13 20:51:23 +0000

    [Bot] Update files - Homebank

--- homebank/tools/chocolateyinstall.ps1
+++ homebank/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
-﻿$packageName = 'homebank'
+$packageName = 'homebank'
 $installerType = 'EXE'
 $url = 'http://homebank.free.fr/public/HomeBank-5.5.3-setup.exe'
-$checksum = 'ba88e3798f705531466ac67a89be9b85a02209c442c963df8e9f837ea2aa71f7'
+$checksum = 'BA88E3798F705531466AC67A89BE9B85A02209C442C963DF8E9F837EA2AA71F7'
 $checkumType = 'sha256'
 $silentArgs = '/verysilent /allusers'
 $validExitCodes = @(0)
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -checksum $checksum -checksumType $checkumType -validExitCodes $validExitCodes
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -checksum $checksum -checksumType $checkumType -validExitCodes $validExitCodes 
