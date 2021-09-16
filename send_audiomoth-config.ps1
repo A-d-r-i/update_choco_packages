1bb66381773c5b42b9c80abda4f4121f505ba7db
A-d-r-i <a-d-r-i@outlook.fr>
2021-09-16 15:29:19 +0200

    Update send_audiomoth-config.ps1

--- send_audiomoth-config.ps1
+++ send_audiomoth-config.ps1
@@ -1,9 +1,11 @@
 $tag = (Invoke-WebRequest "https://api.github.com/repos/OpenAcousticDevices/AudioMoth-Configuration-App/releases/latest" | ConvertFrom-Json)[0].name
+$release = (Invoke-WebRequest "https://api.github.com/repos/OpenAcousticDevices/AudioMoth-Configuration-App/releases/latest" | ConvertFrom-Json)[0].body
 
 $file = "./audiomoth-config/audiomoth-config.nuspec"
 $xml = New-Object XML
 $xml.Load($file)
 $xml.package.metadata.version = $tag
+$xml.package.metadata.releaseNotes = $release
 $xml.Save($file)
 
 Invoke-WebRequest -Uri "https://github.com/OpenAcousticDevices/AudioMoth-Configuration-App/releases/download/$tag/AudioMothConfigurationAppSetup$tag.exe" -OutFile "./audiomoth-config/tools/AudioMothConfigurationAppSetup.exe"
