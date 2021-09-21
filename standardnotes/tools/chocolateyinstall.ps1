e9c234bf3824f9beb6514746418e24a62f04df90
A-d-r-i <a-d-r-i@outlook.fr>
2021-09-21 15:18:37 +0200

    Update chocolateyinstall.ps1

--- standardnotes/tools/chocolateyinstall.ps1
+++ standardnotes/tools/chocolateyinstall.ps1
@@ -1,4 +1,4 @@
-$packageName = 'standardnotes-desktop'
+$packageName = 'standardnotes'
 $installerType = 'EXE'
 $url = 'https://github.com/standardnotes/desktop/releases/download/v3.8.21/standard-notes-3.8.21-win.exe'
 $checksum = 'E3B2AA6F1F181469A79F4681EC6DB74BCA84F8D85699CD5199A00A8DD22B4C59'
