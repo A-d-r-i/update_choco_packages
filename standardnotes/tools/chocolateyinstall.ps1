03ad251fb5a9a45b0259d41c0a429376207fa495
A-d-r-i <27277698+A-d-r-i@users.noreply.github.com>
2021-09-21 13:10:18 +0000

    [Bot] Update files - Standard Notes

--- standardnotes/tools/chocolateyinstall.ps1
+++ standardnotes/tools/chocolateyinstall.ps1
@@ -1,9 +1,8 @@
-﻿$packageName = 'standardnotes-desktop'
+$packageName = 'standardnotes-desktop'
 $installerType = 'EXE'
 $url = 'https://github.com/standardnotes/desktop/releases/download/v3.8.21/standard-notes-3.8.21-win.exe'
-$checksum = '0DD0397EAE51140D07A8CA3E4FDB7B0A1F1599F835CCFE1EADDAC76D82BCC638'
+$checksum = 'E3B2AA6F1F181469A79F4681EC6DB74BCA84F8D85699CD5199A00A8DD22B4C59'
 $checkumType = 'sha256'
-
 $silentArgs = '/S'
 $validExitCodes = @(0)
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -checksum $checksum -checksumType $checkumType -validExitCodes $validExitCodes
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -checksum $checksum -checksumType $checkumType -validExitCodes $validExitCodes 
