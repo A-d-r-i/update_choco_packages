376b9e9a7a2284323a5ffbeeef78174772fd317b
A-d-r-i <a-d-r-i@outlook.fr>
2021-09-27 15:12:01 +0200

    Update chocolateyinstall.ps1

--- tenacity/tools/chocolateyinstall.ps1
+++ tenacity/tools/chocolateyinstall.ps1
@@ -1,20 +1,23 @@
 $ErrorActionPreference = 'Stop'
+
 $toolsDir = Split-Path $MyInvocation.MyCommand.Definition
+
 $packageArgs = @{
   packageName    = 'tenacity'
   fileType       = 'exe'
-  file           = "\tenacity-win-3.0.4-x86.exe"
-  file64         = "\tenacity-win-3.0.4-x64.exe"
+  file           = "$toolsDir\tenacity-win-3.0.4-x86.exe"
+  file64         = "$toolsDir\tenacity-win-3.0.4-x64.exe"
   silentArgs     = '/VERYSILENT'
   validExitCodes = @(0, 1223)
 }
 Install-ChocolateyInstallPackage @packageArgs
-Get-ChildItem "\*.$($packageArgs.fileType)" | ForEach-Object {
+Get-ChildItem "$toolsDir\*.$($packageArgs.fileType)" | ForEach-Object {
   Remove-Item $_ -ea 0
   if (Test-Path $_) {
-    Set-Content ".ignore"
+    Set-Content "$_.ignore"
   }
 }
+
 $packageName = $packageArgs.packageName
 $installLocation = Get-AppInstallLocation $packageName
 if ($installLocation) {
@@ -22,4 +25,4 @@ if ($installLocation) {
   Register-Application "$installLocation\$packageName.exe"
   Write-Host "$packageName registered as $packageName"
 }
-else { Write-Warning "Can't find $PackageName install location" } 
+else { Write-Warning "Can't find $PackageName install location" }
