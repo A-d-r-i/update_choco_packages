bac703a814777c81591279c6acc806c380e4a29b
A-d-r-i <a-d-r-i@outlook.fr>
2021-09-27 14:59:15 +0200

    Add files via upload

--- send_tenacity.ps1
+++ send_tenacity.ps1
@@ -17,20 +17,30 @@ Expand-Archive tenacity64.zip -DestinationPath .\tenacity\tools\ -Force
 Expand-Archive tenacity32.zip -DestinationPath .\tenacity\tools\ -Force
 
 $content = "`$ErrorActionPreference = 'Stop'
-`$toolsDir = `"`$(Split-Path -parent `$MyInvocation.MyCommand.Definition)`"
-`$filePath = if ((Get-OSArchitectureWidth 64) -and `$env:chocolateyForceX86 -ne `$true) {
-       Write-Host `"Installing 64 bit version`" ; Get-Item `$toolsDir\tenacity-win-3.0.4-x64.exe }
-else { Write-Host `"Installing 32 bit version`" ; Get-Item `$toolsDir\tenacity-win-3.0.4-x86.exe }
+`$toolsDir = Split-Path `$MyInvocation.MyCommand.Definition
 `$packageArgs = @{
   packageName    = 'tenacity'
   fileType       = 'exe'
-  softwareName   = 'tenacity*'
-  file           = $filePath
-  silentArgs     = '/S'
-  validExitCodes = @(0)
+  file           = `"$toolsDir\tenacity-win-3.0.4-x86.exe`"
+  file64         = `"$toolsDir\tenacity-win-3.0.4-x64.exe`"
+  silentArgs     = '/VERYSILENT'
+  validExitCodes = @(0, 1223)
 }
 Install-ChocolateyInstallPackage @packageArgs
-Remove-Item `$toolsDir\*.exe -ea 0 -force " | out-file -filepath ./tenacity/tools/chocolateyinstall.ps1
+Get-ChildItem `"$toolsDir\*.`$(`$packageArgs.fileType)`" | ForEach-Object {
+  Remove-Item `$_ -ea 0
+  if (Test-Path `$_) {
+    Set-Content `"$_.ignore`"
+  }
+}
+`$packageName = `$packageArgs.packageName
+`$installLocation = Get-AppInstallLocation `$packageName
+if (`$installLocation) {
+  Write-Host `"`$packageName installed to '`$installLocation'`"
+  Register-Application `"`$installLocation\`$packageName.exe`"
+  Write-Host `"`$packageName registered as `$packageName`"
+}
+else { Write-Warning `"Can't find `$PackageName install location`" } " | out-file -filepath ./tenacity/tools/chocolateyinstall.ps1
 
 Remove-Item tenacity64.zip
 Remove-Item tenacity32.zip
