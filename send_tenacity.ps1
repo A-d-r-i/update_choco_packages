7398a15da060519187857ce830cfe0f952d803b7
A-d-r-i <27277698+A-d-r-i@users.noreply.github.com>
2021-09-27 16:52:06 +0200

    Update send_tenacity.ps1

--- send_tenacity.ps1
+++ send_tenacity.ps1
@@ -1,7 +1,7 @@
 # $tag = (Invoke-WebRequest "https://api.github.com/repos/tenacityteam/tenacity/releases/latest" | ConvertFrom-Json)[0].name
 # $release = (Invoke-WebRequest "https://api.github.com/repos/tenacityteam/tenacity/releases/latest" | ConvertFrom-Json)[0].body
 
-$tag = "0.1.0-beta"
+$tag = "0.1.0-alpha"
 
 $file = "./tenacity/tenacity.nuspec"
 $xml = New-Object XML
