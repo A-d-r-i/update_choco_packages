1f1cbc60ae1833e2298e06c2f497936411ce3bae
A-d-r-i <27277698+A-d-r-i@users.noreply.github.com>
2021-09-29 20:57:08 +0200

    Add files via upload

---
+++ ctemplar/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Join-Path $toolsDir 'ctemplar.exe'
+
+
+$packageArgs = @{
+  packageName   = 'ctemplar'
+  unzipLocation = $toolsDir
+  file           = $fileLocation
+  fileType      = 'EXE'
+  silentArgs     = '/S'
+  softwareName  = 'ctemplar*'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyInstallPackage @packageArgs
+Remove-Item $toolsDir\*.exe -ea 0 -force
