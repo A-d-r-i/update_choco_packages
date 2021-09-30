692bbc63b68d73699ca0e59fb144cce81b94e2fa
A-d-r-i <a-d-r-i@outlook.fr>
2021-09-30 11:53:04 +0200

    Update send_ctemplar.ps1

--- send_ctemplar.ps1
+++ send_ctemplar.ps1
@@ -13,7 +13,52 @@ Invoke-WebRequest -Uri "https://github.com/CTemplar/webclient/releases/download/
 choco pack ./ctemplar/ctemplar.nuspec --outputdirectory .\ctemplar
 
 If ($LastExitCode -eq 0) {
-	choco push ./ctemplar/ctemplar.$tag.nupkg --source https://push.chocolatey.org/
+	#choco push ./ctemplar/ctemplar.$tag.nupkg --source https://push.chocolatey.org/
 } else {
  'Error - Exit code: $LastExitCode'
+}
+
+#git and create tag
+git config --local user.email "a-d-r-i@outlook.fr"
+git config --local user.name "A-d-r-i"
+git add .
+git commit -m "[Bot] Update files - ctemplar" --allow-empty
+git tag -a ctemplar-v$tag -m "CTemplar - version $tag"
+git push -f && git push --tags
+
+#create release
+Install-Module -Name New-GitHubRelease -Force
+Import-Module -Name New-GitHubRelease
+$newGitHubReleaseParameters = @{
+GitHubUsername = "A-d-r-i"
+GitHubRepositoryName = "update_choco_package"
+GitHubAccessToken = "$env:ACTIONS_TOKEN"
+ReleaseName = "CTemplar v$tag"
+TagName = "ctemplar-v$tag"
+ReleaseNotes = "$release"
+AssetFilePaths = ".\ctemplar\ctemplar.$tag.nupkg"
+IsPreRelease = $false
+IsDraft = $false
+}
+$resultrelease = New-GitHubRelease @newGitHubReleaseParameters
+
+#post tweet
+$twitter = (Select-String -Path config.txt -Pattern "twitter=(.*)").Matches.Groups[1].Value
+if ( $twitter -eq "y" )
+{
+Install-Module PSTwitterAPI -Force
+Import-Module PSTwitterAPI
+$OAuthSettings = @{
+ApiKey = "${{ secrets.PST_KEY }}"
+ApiSecret = "${{ secrets.PST_SECRET }}"
+AccessToken = "${{ secrets.PST_TOKEN }}"
+AccessTokenSecret = "${{ secrets.PST_TOKEN_SECRET }}"
+}
+Set-TwitterOAuthSettings @OAuthSettings
+Send-TwitterStatuses_Update -status "CTemplar v$tag push now on @chocolateynuget! 
+
+Link: https://community.chocolatey.org/packages/ctemplar/$tag
+@RealCTemplar
+#ctemplar #release #opensource
+"
 }
