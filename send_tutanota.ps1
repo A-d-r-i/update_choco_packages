a089a0c1c5d0bee1d50dbe8f0dce20b7592231a1
A-d-r-i <a-d-r-i@outlook.fr>
2021-09-30 12:10:53 +0200

    Update send_tutanota.ps1

--- send_tutanota.ps1
+++ send_tutanota.ps1
@@ -20,3 +20,48 @@ If ($LastExitCode -eq 0) {
 } else {
  'Error - Exit code: $LastExitCode'
 }
+
+#git and create tag
+git config --local user.email "a-d-r-i@outlook.fr"
+git config --local user.name "A-d-r-i"
+git add .
+git commit -m "[Bot] Update files - Tutanota" --allow-empty
+git tag -a tutanota-v$tag -m "Tutanota - version $tag"
+git push -f && git push --tags
+
+#create release
+Install-Module -Name New-GitHubRelease -Force
+Import-Module -Name New-GitHubRelease
+$newGitHubReleaseParameters = @{
+GitHubUsername = "A-d-r-i"
+GitHubRepositoryName = "update_choco_package"
+GitHubAccessToken = "$env:ACTIONS_TOKEN"
+ReleaseName = "Tutanota v$tag"
+TagName = "tutanota-v$tag"
+ReleaseNotes = "$release"
+AssetFilePaths = ".\tutanota\tutanota.$tag.nupkg"
+IsPreRelease = $false
+IsDraft = $false
+}
+$resultrelease = New-GitHubRelease @newGitHubReleaseParameters
+
+#post tweet
+$twitter = (Select-String -Path config.txt -Pattern "twitter=(.*)").Matches.Groups[1].Value
+if ( $twitter -eq "y" )
+{
+Install-Module PSTwitterAPI -Force
+Import-Module PSTwitterAPI
+$OAuthSettings = @{
+ApiKey = "${{ secrets.PST_KEY }}"
+ApiSecret = "${{ secrets.PST_SECRET }}"
+AccessToken = "${{ secrets.PST_TOKEN }}"
+AccessTokenSecret = "${{ secrets.PST_TOKEN_SECRET }}"
+}
+Set-TwitterOAuthSettings @OAuthSettings
+Send-TwitterStatuses_Update -status "Tutanota v$tag push now on @chocolateynuget! 
+
+Link: https://community.chocolatey.org/packages/tutanota/$tag
+@TutanotaTeam
+#tutanota #release #opensource
+"
+}
