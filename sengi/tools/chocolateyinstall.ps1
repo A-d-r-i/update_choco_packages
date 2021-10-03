3edd640e76f56799f6b1528b172e1a6d7c4b0f34
A-d-r-i <a-d-r-i@outlook.fr>
2021-10-03 18:30:34 +0200

    Add files via upload

---
+++ sengi/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Join-Path $toolsDir 'sengi.exe'
+
+
+$packageArgs = @{
+  packageName   = 'sengi'
+  unzipLocation = $toolsDir
+  file           = $fileLocation
+  fileType      = 'EXE'
+  silentArgs     = '/S'
+  softwareName  = 'sengi*'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyInstallPackage @packageArgs
+Remove-Item $toolsDir\*.exe -ea 0 -force
