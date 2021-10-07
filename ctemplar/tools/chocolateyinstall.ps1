5c42f8734de616c1f18bb5fa949ef25d0a2f79bc
A-d-r-i <27277698+A-d-r-i@users.noreply.github.com>
2021-10-07 12:01:32 +0200

    Update chocolateyinstall.ps1

--- ctemplar/tools/chocolateyinstall.ps1
+++ ctemplar/tools/chocolateyinstall.ps1
@@ -3,5 +3,5 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageName= 'ctemplar'
 
 Install-BinFile -Name ctemplar -Path "$($env:ChocolateyInstall)\bin"
-Install-ChocolateyShortcut -ShortcutFilePath "$($env:SystemDrive)\ProgramData\Microsoft\Windows\Start Menu\Programs\ctemplar.lnk" -TargetPath "$($env:ChocolateyInstall)\bin\ctemplar.exe"
-Install-ChocolateyShortcut -ShortcutFilePath "$([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::DesktopDirectory))\ctemplar.lnk" -TargetPath "$($env:ChocolateyInstall)\bin\ctemplar.exe"
+Install-ChocolateyShortcut -ShortcutFilePath "$($env:SystemDrive)\ProgramData\Microsoft\Windows\Start Menu\Programs\CTemplar.lnk" -TargetPath "$($env:ChocolateyInstall)\bin\ctemplar.exe"
+Install-ChocolateyShortcut -ShortcutFilePath "$([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::DesktopDirectory))\CTemplar.lnk" -TargetPath "$($env:ChocolateyInstall)\bin\ctemplar.exe"
