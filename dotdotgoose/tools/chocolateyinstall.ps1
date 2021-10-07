56220a53556659bb648cab41a7712f9c6621cc71
A-d-r-i <a-d-r-i@outlook.fr>
2021-10-07 16:01:48 +0200

    Add files via upload

---
+++ dotdotgoose/tools/chocolateyinstall.ps1
@@ -0,0 +1,8 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$packageName = 'dotdotgoose'
+
+Install-BinFile -Name $packageName -Path "$($env:ChocolateyInstall)\bin"
+Install-ChocolateyShortcut -ShortcutFilePath "$($env:SystemDrive)\ProgramData\Microsoft\Windows\Start Menu\Programs\DotDotGoose.lnk" -TargetPath "$($env:ChocolateyInstall)\bin\DotDotGoose.exe"
+Install-ChocolateyShortcut -ShortcutFilePath "$([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::DesktopDirectory))\DotDotGoose.lnk" -TargetPath "$($env:ChocolateyInstall)\bin\DotDotGoose.exe"
