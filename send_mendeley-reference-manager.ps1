c2ff710d839fe5d3a72fb10e0ec239e31aa5361b
A-d-r-i <27277698+A-d-r-i@users.noreply.github.com>
2021-10-07 16:13:29 +0200

    Update send_mendeley-reference-manager.ps1

--- send_mendeley-reference-manager.ps1
+++ send_mendeley-reference-manager.ps1
@@ -1,7 +1,6 @@
 Invoke-WebRequest -Uri "https://www.mendeley.com/release-notes-reference-manager" -OutFile "MRM.html"
-Invoke-WebRequest -Uri "https://www.mendeley.com/release-notes-reference-manager" -OutFile "release.html"
-$Source = Get-Content -path MRM.html -raw
-$text = Get-Content -path release.html
+$Source = Get-Content -path MRM.html
+$text = Get-Content -path MRM.html
 $Source -match '<div class="views-field views-field-title"><span class="field-content"><a href="/release-notes-reference-manager/v([0-9]+(\.[0-9]+)+)">Reference Manager'
 $tag = $matches[1]
 
