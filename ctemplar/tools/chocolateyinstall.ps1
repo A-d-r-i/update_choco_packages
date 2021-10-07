e0da2185ab96c25a1f181a4ac40fad9aab27ae6f
A-d-r-i <a-d-r-i@outlook.fr>
2021-10-07 15:53:41 +0200

    Update chocolateyinstall.ps1

--- ctemplar/tools/chocolateyinstall.ps1
+++ ctemplar/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$packageName= 'ctemplar'
+$packageName = 'ctemplar'
 
-Install-BinFile -Name ctemplar -Path "$($env:ChocolateyInstall)\bin"
+Install-BinFile -Name $packageName -Path "$($env:ChocolateyInstall)\bin"
 Install-ChocolateyShortcut -ShortcutFilePath "$($env:SystemDrive)\ProgramData\Microsoft\Windows\Start Menu\Programs\CTemplar.lnk" -TargetPath "$($env:ChocolateyInstall)\bin\ctemplar.exe"
 Install-ChocolateyShortcut -ShortcutFilePath "$([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::DesktopDirectory))\CTemplar.lnk" -TargetPath "$($env:ChocolateyInstall)\bin\ctemplar.exe"
