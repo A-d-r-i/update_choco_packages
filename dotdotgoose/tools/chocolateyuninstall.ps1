71305e9f4c29857a00be52a7da2d6ae9b265c966
A-d-r-i <27277698+A-d-r-i@users.noreply.github.com>
2021-10-18 14:33:59 +0200

    Update chocolateyuninstall.ps1

--- dotdotgoose/tools/chocolateyuninstall.ps1
+++ dotdotgoose/tools/chocolateyuninstall.ps1
@@ -1,3 +1,6 @@
-﻿$ErrorActionPreference = 'Stop';
+$ErrorActionPreference = 'Stop';
+
+Remove-Item -Path "$($env:SystemDrive)\ProgramData\Microsoft\Windows\Start Menu\Programs\DotDotGoose.lnk" -Force
+Remove-Item -Path "$([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::DesktopDirectory))\DotDotGoose.lnk" -Force
 
 Uninstall-BinFile -Name dotdotgoose
