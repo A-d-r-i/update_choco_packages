bfa72d70b6a6ccb12b4c91bddf8f00b95f11abe6
A-d-r-i <a-d-r-i@outlook.fr>
2021-10-18 14:32:44 +0200

    Update chocolateyuninstall.ps1

--- ctemplar/tools/chocolateyuninstall.ps1
+++ ctemplar/tools/chocolateyuninstall.ps1
@@ -1,3 +1,6 @@
 $ErrorActionPreference = 'Stop';
 
+Remove-Item -Path "$($env:SystemDrive)\ProgramData\Microsoft\Windows\Start Menu\Programs\ctemplar.lnk" -Force
+Remove-Item -Path "$([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::DesktopDirectory))\ctemplar.lnk" -Force
+
 Uninstall-BinFile -Name ctemplar
