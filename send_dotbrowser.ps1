4629403a8b8324b275d0e60a96c393c520a30148
A-d-r-i <a-d-r-i@outlook.fr>
2021-10-20 12:00:47 +0200

    Update send_dotbrowser.ps1

--- send_dotbrowser.ps1
+++ send_dotbrowser.ps1
@@ -1,6 +1,7 @@
 $tag = (Invoke-WebRequest "https://api.github.com/repos/dothq/browser-desktop/releases/latest" | ConvertFrom-Json)[0].tag_name
 $release = (Invoke-WebRequest "https://api.github.com/repos/dothq/browser-desktop/releases/latest" | ConvertFrom-Json)[0].body
 
+$tagalpha = $tag -replace '([0-9]*\.[0-9]+)-([0-9]{4})-([0-9]{2})-([0-9]{2})', '$1.$2$3$4'
 $tagalpha = -join($tag,"-alpha");
 
 $file = "./dotbrowser/dotbrowser.nuspec"
