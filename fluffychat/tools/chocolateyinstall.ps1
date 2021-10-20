b18d551bdde3dbde6f08463b0ddceabce5bb667f
A-d-r-i <27277698+A-d-r-i@users.noreply.github.com>
2021-10-20 13:43:10 +0000

    [Bot] Update files - fluffychat

--- fluffychat/tools/chocolateyinstall.ps1
+++ fluffychat/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
-﻿$ErrorActionPreference = 'Stop';
+$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageName = 'fluffychat'
 
-Install-ChocolateyZipPackage -PackageName $packageName`
- -Url 'https://gitlab.com/api/v4/projects/16112282/packages/generic/fluffychat/$tag/fluffychat-windows.zip' `
+Install-ChocolateyZipPackage -PackageName $packageName
+ -Url 'https://gitlab.com/api/v4/projects/16112282/packages/generic/fluffychat/$tag/fluffychat-windows.zip' 
  -UnzipLocation $toolsDir
 
-Install-ChocolateyShortcut -ShortcutFilePath "$($env:SystemDrive)\ProgramData\Microsoft\Windows\Start Menu\Programs\FluffyChat.lnk" -TargetPath "$toolsDir\fluffychat.exe"
-Install-ChocolateyShortcut -ShortcutFilePath "$([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::DesktopDirectory))\FluffyChat.lnk" -TargetPath "$toolsDir\fluffychat.exe"
+Install-ChocolateyShortcut -ShortcutFilePath "$($env:SystemDrive)\ProgramData\Microsoft\Windows\Start Menu\Programs\FluffyChat.lnk" -TargetPath "\fluffychat.exe"
+Install-ChocolateyShortcut -ShortcutFilePath "$([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::DesktopDirectory))\FluffyChat.lnk" -TargetPath "$toolsDir\fluffychat.exe" 
