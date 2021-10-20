1d2eb9c2b0fb53f9698fcec10b567795074a5142
A-d-r-i <27277698+A-d-r-i@users.noreply.github.com>
2021-10-20 13:53:26 +0000

    [Bot] Update files - fluffychat

--- fluffychat/tools/chocolateyinstall.ps1
+++ fluffychat/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageName = 'fluffychat'
 
 Install-ChocolateyZipPackage -PackageName $packageName
- -Url 'https://gitlab.com/api/v4/projects/16112282/packages/generic/fluffychat/$tag/fluffychat-windows.zip' 
+ -Url 'https://gitlab.com/api/v4/projects/16112282/packages/generic/fluffychat/0.42.0/fluffychat-windows.zip' 
  -UnzipLocation $toolsDir
 
 Install-ChocolateyShortcut -ShortcutFilePath "$($env:SystemDrive)\ProgramData\Microsoft\Windows\Start Menu\Programs\FluffyChat.lnk" -TargetPath "\fluffychat.exe"
