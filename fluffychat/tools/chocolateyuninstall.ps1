88bf5b0ec4057c6cabd70ac792b1bcaf2cdc9f9e
A-d-r-i <27277698+A-d-r-i@users.noreply.github.com>
2021-10-20 15:25:49 +0200

    Add files via upload

---
+++ fluffychat/tools/chocolateyuninstall.ps1
@@ -0,0 +1,4 @@
+﻿$ErrorActionPreference = 'Stop';
+
+Remove-Item -Path "$($env:SystemDrive)\ProgramData\Microsoft\Windows\Start Menu\Programs\FluffyChat.lnk" -Force
+Remove-Item -Path "$([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::DesktopDirectory))\FluffyChat.lnk" -Force
