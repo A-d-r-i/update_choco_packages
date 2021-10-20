6406b42bc73458d24c6f078f58a2b7a9cecb6d2e
A-d-r-i <a-d-r-i@outlook.fr>
2021-10-20 15:25:49 +0200

    Add files via upload

---
+++ send_fluffychat.ps1
@@ -0,0 +1,77 @@
+$tag = (Invoke-WebRequest "https://gitlab.com/api/v4/projects/16112282/releases" | ConvertFrom-Json)[0].tag_name
+$tag = $tag -replace 'v'
+
+Invoke-WebRequest -Uri "https://gitlab.com/famedly/fluffychat/-/raw/main/CHANGELOG.md" -OutFile "FC.md"
+$text = Get-Content -path FC.md
+$pattern = '##(.*?)##'
+$result = [regex]::match($text, $pattern).Groups[1].Value
+$release = $result -replace '^','##'
+$release = $release -replace "(- [0-9]{4}-[0-9]{2}-[0-9]{2} )","$&`n"
+$release = $release -replace "- [a-zA-Z]","`n $&"
+$release = -join($release, "`n`n**Full changelog:** [https://gitlab.com/famedly/fluffychat/-/blob/main/CHANGELOG.md](https://gitlab.com/famedly/fluffychat/-/blob/main/CHANGELOG.md) ");
+
+$file = "./fluffychat/fluffychat.nuspec"
+$xml = New-Object XML
+$xml.Load($file)
+$xml.package.metadata.version = $tag
+$xml.package.metadata.releaseNotes = $release
+$xml.Save($file)
+
+Invoke-WebRequest -Uri "https://github.com/dothq/browser-desktop/releases/download/$tag/dot-87.0.exe" -OutFile "./fluffychat/tools/fluffychat.exe"
+
+choco pack ./fluffychat/fluffychat.nuspec --outputdirectory .\fluffychat
+
+If ($LastExitCode -eq 0) {
+	choco push ./fluffychat/fluffychat.$tag.nupkg --source https://push.chocolatey.org/
+} else {
+	echo "Error in introduction - Exit code: $LastExitCode "
+}
+
+If ($LastExitCode -eq 0) {
+
+#git and create tag
+git config --local user.email "a-d-r-i@outlook.fr"
+git config --local user.name "A-d-r-i"
+git add .
+git commit -m "[Bot] Update files - fluffychat" --allow-empty
+git tag -a fluffychat-v$tag -m "FluffyChat  - version $tag"
+git push -f && git push --tags
+
+#create release
+Install-Module -Name New-GitHubRelease -Force
+Import-Module -Name New-GitHubRelease
+$newGitHubReleaseParameters = @{
+GitHubUsername = "A-d-r-i"
+GitHubRepositoryName = "update_choco_package"
+GitHubAccessToken = "$env:ACTIONS_TOKEN"
+ReleaseName = "FluffyChat v$tag"
+TagName = "fluffychat-v$tag"
+ReleaseNotes = "$release"
+AssetFilePaths = ".\fluffychat\fluffychat.$tag.nupkg"
+IsPreRelease = $false
+IsDraft = $false
+}
+$resultrelease = New-GitHubRelease @newGitHubReleaseParameters
+
+#post tweet
+$twitter = (Select-String -Path config.txt -Pattern "twitter=(.*)").Matches.Groups[1].Value
+if ( $twitter -eq "y" )
+{
+Install-Module PSTwitterAPI -Force
+Import-Module PSTwitterAPI
+$OAuthSettings = @{
+ApiKey = "$env:PST_KEY"
+ApiSecret = "$env:PST_SECRET"
+AccessToken = "$env:PST_TOKEN"
+AccessTokenSecret = "$env:PST_TOKEN_SECRET"
+}
+Set-TwitterOAuthSettings @OAuthSettings
+Send-TwitterStatuses_Update -status "FluffyChat v$tag push now on @chocolateynuget! 
+
+Link: https://community.chocolatey.org/packages/fluffychat/$tag
+#fluffychat #release #opensource
+"
+}
+} else {
+	echo "Error in choco push - Exit code: $LastExitCode "
+}
