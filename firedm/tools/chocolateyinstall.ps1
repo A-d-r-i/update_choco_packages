61d4ae180f83c8f16af6340bf7ada7cedac793c5
A-d-r-i <a-d-r-i@outlook.fr>
2021-10-30 09:47:01 +0000

    [Bot] Update files - firedm

--- firedm/tools/chocolateyinstall.ps1
+++ firedm/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$packageName = 'fluffychat'
+$packageName = 'firedm'
 
-Install-ChocolateyZipPackage -PackageName $packageName -Url 'https://gitlab.com/api/v4/projects/16112282/packages/generic/fluffychat/0.42.0/fluffychat-windows.zip' -UnzipLocation $toolsDir
+Install-ChocolateyZipPackage -PackageName $packageName -Url 'https://github.com/firedm/FireDM/releases/download/2021.9.28/FireDM_2021.9.28.zip' -UnzipLocation $toolsDir
 
-Install-ChocolateyShortcut -ShortcutFilePath "$($env:SystemDrive)\ProgramData\Microsoft\Windows\Start Menu\Programs\FluffyChat.lnk" -TargetPath "$toolsDir\fluffychat.exe"
-Install-ChocolateyShortcut -ShortcutFilePath "$([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::DesktopDirectory))\FluffyChat.lnk" -TargetPath "$toolsDir\fluffychat.exe" 
+Install-ChocolateyShortcut -ShortcutFilePath "$($env:SystemDrive)\ProgramData\Microsoft\Windows\Start Menu\Programs\FireDM.lnk" -TargetPath "$toolsDir\FireDM\firedm.exe"
+Install-ChocolateyShortcut -ShortcutFilePath "$([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::DesktopDirectory))\FireDM.lnk" -TargetPath "$toolsDir\FireDM\firedm.exe" 
