fa1cfe5ec69bec36cba16c4303be10a616388968
A-d-r-i <27277698+A-d-r-i@users.noreply.github.com>
2021-11-08 12:57:47 +0000

    [Bot] Update files - firedm

--- firedm/tools/chocolateyinstall.ps1
+++ firedm/tools/chocolateyinstall.ps1
@@ -1,10 +1,15 @@
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$packageName = 'firedm'
-$checksum = '8CBD695820DB70477B4BA9BD70B86F2A917EC9F0E2EB5663B29AD7ABA232B54B'
-$checkumType = 'sha256'
 
-Install-ChocolateyZipPackage -PackageName $packageName -checksum $checksum -checksumType $checkumType -Url 'https://github.com/firedm/FireDM/releases/download/2021.9.28/FireDM_2021.9.28.zip' -UnzipLocation $toolsDir
+$packageArgs = @{
+  packageName   = 'firedm'
+  checksum = '8A40A150EAE6E677716CBB9307207875CE9AF2E5AAC0B846FA448314C2D960B9'
+  checksumType = 'sha256'
+  Url = 'https://github.com/firedm/FireDM/releases/download/2021.11.4/FireDM_2021.11.4.zip'
+  UnzipLocation = $toolsDir
+}
+
+Install-ChocolateyZipPackage @packageArgs
 
 Install-ChocolateyShortcut -ShortcutFilePath "$($env:SystemDrive)\ProgramData\Microsoft\Windows\Start Menu\Programs\FireDM.lnk" -TargetPath "$toolsDir\FireDM\firedm.exe"
 Install-ChocolateyShortcut -ShortcutFilePath "$([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::DesktopDirectory))\FireDM.lnk" -TargetPath "$toolsDir\FireDM\firedm.exe" 
