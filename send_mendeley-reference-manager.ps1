44ba28729555bcef8fba2be5c2c55ba27107dca5
A-d-r-i <27277698+A-d-r-i@users.noreply.github.com>
2021-11-24 10:11:07 +0100

    Update delete of temp files

--- send_mendeley-reference-manager.ps1
+++ send_mendeley-reference-manager.ps1
@@ -24,6 +24,9 @@ $xml.Save($file)
 
 Invoke-WebRequest -Uri "https://static.mendeley.com/bin/desktop/mendeley-reference-manager-$tag.exe" -OutFile "./mendeley-reference-manager/tools/mendeley-reference-manager.exe"
 
+Remove-Item MRM.txt
+Remove-Item MRM.html
+
 choco pack ./mendeley-reference-manager/mendeley-reference-manager.nuspec --outputdirectory .\mendeley-reference-manager
 
 If ($LastExitCode -eq 0) {
