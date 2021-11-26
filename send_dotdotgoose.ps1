b4abf7b59a7add4305a299222f0ca6d1add9c705
A-d-r-i <27277698+A-d-r-i@users.noreply.github.com>
2021-11-26 13:24:12 +0100

    Update send_dotdotgoose.ps1

--- send_dotdotgoose.ps1
+++ send_dotdotgoose.ps1
@@ -1,31 +1,31 @@
-#Invoke-WebRequest -Uri "https://biodiversityinformatics.amnh.org/open_source/dotdotgoose/index.html" -OutFile "DDG.html"
-#Invoke-WebRequest -Uri "https://biodiversityinformatics.amnh.org/open_source/dotdotgoose/index.html" -OutFile "release.html"
-#$Source = Get-Content -path DDG.html
-#$text = Get-Content -path release.html
-#$Source -match '<ul class="local-list"> <li>[0-9]{4}-[0-9]{2}-[0-9]{2} - version ([0-9]+(\.[0-9]+)+) '
-#$tag = $matches[1]
+Invoke-WebRequest -Uri "https://biodiversityinformatics.amnh.org/open_source/dotdotgoose/index.html" -OutFile "DDG.html"
+Invoke-WebRequest -Uri "https://biodiversityinformatics.amnh.org/open_source/dotdotgoose/index.html" -OutFile "release.html"
+$Source = Get-Content -path DDG.html
+$text = Get-Content -path release.html
+$Source -match '<ul class="local-list"> <li>[0-9]{4}-[0-9]{2}-[0-9]{2} - version ([0-9]+(\.[0-9]+)+) '
+$tag = $matches[1]
 
-$tag = "1.5.2"
+#$tag = "1.5.2"
 
-#$pattern = '<ul class="local-list"> <li>(.*?)</li> </ul> </li>'
-#$result = [regex]::match($text, $pattern).Groups[1].Value
+$pattern = '<ul class="local-list"> <li>(.*?)</li> </ul> </li>'
+$result = [regex]::match($text, $pattern).Groups[1].Value
 
-#$release = $result -replace ' <ul class="local-list"> <li>', "`n* "
-#$release = $release -replace '</li> <li>', "`n* "
-#$release = $release -replace '(Enhancement)', '**Enhancement**'
-#$release = $release -replace '(Bug Fix)', '**Bug Fix**'
-#$release = $release -replace '(Bug Fixed)', '**Bug Fixed**'
-#$release = -join($release, "`n`n**Full changelog:** [https://biodiversityinformatics.amnh.org/open_source/dotdotgoose/](https://biodiversityinformatics.amnh.org/open_source/dotdotgoose/) ");
+$release = $result -replace ' <ul class="local-list"> <li>', "`n* "
+$release = $release -replace '</li> <li>', "`n* "
+$release = $release -replace '(Enhancement)', '**Enhancement**'
+$release = $release -replace '(Bug Fix)', '**Bug Fix**'
+$release = $release -replace '(Bug Fixed)', '**Bug Fixed**'
+$release = -join($release, "`n`n**Full changelog:** [https://biodiversityinformatics.amnh.org/open_source/dotdotgoose/](https://biodiversityinformatics.amnh.org/open_source/dotdotgoose/) ");
 
 
 $file = "./dotdotgoose/dotdotgoose.nuspec"
 $xml = New-Object XML
 $xml.Load($file)
 $xml.package.metadata.version = $tag
-$xml.package.metadata.releaseNotes = " https://biodiversityinformatics.amnh.org/open_source/dotdotgoose/ " # $release
+$xml.package.metadata.releaseNotes = "$release"# https://biodiversityinformatics.amnh.org/open_source/dotdotgoose/ " # $release
 $xml.Save($file)
 
-Invoke-WebRequest -Uri "https://adri-server.ddns.net/CDN/A-D-R-I/dotdotgoose-win.zip" -OutFile "dotdotgoose.zip"
+Invoke-WebRequest -Uri "https://biodiversityinformatics.amnh.org/open_source/dotdotgoose/ddg.php?op=download-win" -OutFile "dotdotgoose.zip"
 
 Expand-Archive dotdotgoose.zip -DestinationPath .\dotdotgoose\tools\ -Force
 
