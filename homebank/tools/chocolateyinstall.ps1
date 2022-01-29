b9d076ddf3be5f4da2f0ecaa39337ecc0defb99e
A-d-r-i <a-d-r-i@outlook.fr>
2022-01-29 17:15:29 +0100

    Correction of HomeBank

--- homebank/tools/chocolateyinstall.ps1
+++ homebank/tools/chocolateyinstall.ps1
@@ -1,13 +1,17 @@
 $ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Join-Path $toolsDir 'homebank.exe'
+
 
 $packageArgs = @{
-  packageName = 'homebank'
-  installerType = 'EXE'
-  url = 'http://homebank.free.fr/public/HomeBank-5.5.4-setup.exe'
-  checksum = 'D105FB34B9A78C0FACE50CCF718F778FEF5A55167BF23BD03E9932206E9C9881'
-  checkumType = 'sha256'
-  silentArgs = '/verysilent /allusers'
-  validExitCodes = @(0)
+  packageName   = 'homebank'
+  unzipLocation = $toolsDir
+  file           = $fileLocation
+  fileType      = 'EXE'
+  silentArgs     = '/verysilent /allusers'
+  softwareName  = 'homebank*'
+  validExitCodes= @(0)
 }
 
-Install-ChocolateyInstallPackage @packageArgs 
+Install-ChocolateyInstallPackage @packageArgs
+Remove-Item $toolsDir\*.exe -ea 0 -force
