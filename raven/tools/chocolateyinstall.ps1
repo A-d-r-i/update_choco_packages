55b269f07179c718ce8622a839759c0f8d5f1428
A-d-r-i <27277698+A-d-r-i@users.noreply.github.com>
2022-01-29 17:47:49 +0100

    Update of Raven

--- raven/tools/chocolateyinstall.ps1
+++ raven/tools/chocolateyinstall.ps1
@@ -1,13 +1,17 @@
 $ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Join-Path $toolsDir 'raven.exe'
+
 
 $packageArgs = @{
-  packageName = 'raven'
-  installerType = 'EXE'
-  url = 'https://github.com/hello-efficiency-inc/raven-reader/releases/download/v1.0.70/Raven-Reader-Setup-1.0.70.exe'
-  checksum = '5383A1CF80AE774993CF4567BE26ED6E1E67ADC0A4EA8ADF034FC1245D446A40'
-  checkumType = 'sha256'
-  silentArgs = '/S'
-  validExitCodes = @(0)
+  packageName   = 'raven'
+  unzipLocation = $toolsDir
+  file           = $fileLocation
+  fileType      = 'EXE'
+  silentArgs     = '/S'
+  softwareName  = 'raven*'
+  validExitCodes= @(0)
 }
 
-Install-ChocolateyInstallPackage @packageArgs 
+Install-ChocolateyInstallPackage @packageArgs
+Remove-Item $toolsDir\*.exe -ea 0 -force
