2ea372e7eaf03fb92c55c4cad34e177394663faa
A-d-r-i <27277698+A-d-r-i@users.noreply.github.com>
2022-02-14 16:52:36 +0100

    Update audiomoth VERIFICATION

--- send_audiomoth-flash.ps1
+++ send_audiomoth-flash.ps1
@@ -1,6 +1,8 @@
+# extract latest version and release
 $tag = (Invoke-WebRequest "https://api.github.com/repos/OpenAcousticDevices/AudioMoth-Flash-App/releases/latest" | ConvertFrom-Json)[0].name
 $release = (Invoke-WebRequest "https://api.github.com/repos/OpenAcousticDevices/AudioMoth-Flash-App/releases/latest" | ConvertFrom-Json)[0].body
 
+# write new version and release
 $file = "./audiomoth-flash/audiomoth-flash.nuspec"
 $xml = New-Object XML
 $xml.Load($file)
@@ -8,8 +10,33 @@ $xml.package.metadata.version = $tag
 $xml.package.metadata.releaseNotes = $release
 $xml.Save($file)
 
+# download installer
 Invoke-WebRequest -Uri "https://github.com/OpenAcousticDevices/AudioMoth-Flash-App/releases/download/$tag/AudioMothFlashAppSetup$tag.exe" -OutFile "./audiomoth-flash/tools/AudioMothFlashAppSetup.exe"
 
+# calculation of checksum
+$TABLE = Get-FileHash "./audiomoth-flash/tools/AudioMothFlashAppSetup.exe" -Algorithm SHA256
+$SHA = $TABLE.Hash
+
+# writing of VERIFICATION.txt
+$content = "VERIFICATION
+Verification is intended to assist the Chocolatey moderators and community
+in verifying that this package's contents are trustworthy.
+
+The installer have been downloaded from their official github repository listed on <https://github.com/OpenAcousticDevices/AudioMoth-AudioMoth-Flash-App/releases/>
+and can be verified like this:
+
+1. Download the following installer:
+  Version $tag : <https://github.com/OpenAcousticDevices/AudioMoth-Flash-App/releases/download/$tag/AudioMothFlashAppSetup$tag.exe>
+2. You can use one of the following methods to obtain the checksum
+  - Use powershell function 'Get-Filehash'
+  - Use chocolatey utility 'checksum.exe'
+
+  checksum type: 
+  checksum: $SHA
+
+File 'LICENSE.txt' is obtained from <https://www.openacousticdevices.info/license> " | out-file -filepath ./audiomoth-flash/legal/VERIFICATION.txt
+
+# packaging
 choco pack ./audiomoth-flash/audiomoth-flash.nuspec --outputdirectory .\audiomoth-flash
 
 If ($LastExitCode -eq 0) {
@@ -19,7 +46,7 @@ If ($LastExitCode -eq 0) {
 }
 
 If ($LastExitCode -eq 0) {
-#git and create tag
+# git and create tag
 git config --local user.email "a-d-r-i@outlook.fr"
 git config --local user.name "A-d-r-i"
 git add .
@@ -27,7 +54,7 @@ git commit -m "[Bot] Update files - audiomoth-flash" --allow-empty
 git tag -a audiomoth-flash-v$tag -m "Audiomoth Flash - version $tag"
 git push -f && git push --tags
 
-#create release
+# create release
 Install-Module -Name New-GitHubRelease -Force
 Import-Module -Name New-GitHubRelease
 $newGitHubReleaseParameters = @{
@@ -43,7 +70,7 @@ IsDraft = $false
 }
 $resultrelease = New-GitHubRelease @newGitHubReleaseParameters
 
-#post tweet
+# post tweet
 $twitter = (Select-String -Path config.txt -Pattern "twitter=(.*)").Matches.Groups[1].Value
 if ( $twitter -eq "y" )
 {
@@ -64,7 +91,7 @@ Link: https://community.chocolatey.org/packages/audiomoth-flash/$tag
 "
 }
 
-#send telegram notification
+# send telegram notification
 Function Send-Telegram {
 Param([Parameter(Mandatory=$true)][String]$Message)
 $Telegramtoken = "$env:TELEGRAM"
