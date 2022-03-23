75c225053c4a37db8a7b84269d7e4a24b7039679
A-d-r-i <a-d-r-i@outlook.fr>
2022-03-23 16:42:23 +0100

    Update Tartube without x32

--- tartube/tools/chocolateyinstall.ps1
+++ tartube/tools/chocolateyinstall.ps1
@@ -1,21 +1,12 @@
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-if (Test-Path "tartube32.exe") {
-	$fileLocation = Join-Path $toolsDir 'tartube32.exe'
-}
-else
-{
-	Write-Host "32 bit file doesn't exists"
-	$fileLocation = Join-Path $toolsDir ''
-}
-$fileLocation64 = Join-Path $toolsDir 'tartube64.exe'
+$fileLocation = Join-Path $toolsDir 'tartube64.exe'
 
 $packageArgs = @{
   packageName   = 'tartube'
   unzipLocation = $toolsDir
   file           = $fileLocation
-  file64         = $fileLocation64
   fileType      = 'EXE'
   silentArgs     = '/S'
   softwareName  = 'tartube*'
