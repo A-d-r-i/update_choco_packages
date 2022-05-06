d5df093a849333c7406076d50c98badabcb33c39
A-d-r-i <27277698+A-d-r-i@users.noreply.github.com>
2022-05-06 14:01:31 +0000

    [Bot] Update files - fluffychat

--- fluffychat/tools/chocolateyinstall.ps1
+++ fluffychat/tools/chocolateyinstall.ps1
@@ -3,9 +3,9 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName   = 'fluffychat'
-  checksum = '2757D95E16B76DA496306ACD1C9DEB34102BBF522B09D57378E0E3070CAEC640'
+  checksum = '8C3A267964DBB310A1F31103D711C2AA89D5A527A7A4BC2927E59D0F339B085D'
   checksumType = 'sha256'
-  Url = 'https://gitlab.com/famedly/fluffychat/-/archive/v1.3.1/fluffychat-v1.3.1.zip'
+  Url = 'https://gitlab.com/famedly/fluffychat/-/archive/v1.4.0/fluffychat-v1.4.0.zip'
   UnzipLocation = $toolsDir
 }
 
