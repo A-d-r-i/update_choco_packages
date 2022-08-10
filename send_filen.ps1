6f35e8142b5f944fd26f63c3d837ce6b77008031
A-d-r-i <a-d-r-i@outlook.fr>
2022-08-10 16:21:20 +0200

    Add Filen

---
+++ send_filen.ps1
@@ -0,0 +1,108 @@
+# extract latest version and release
+$tag = (Invoke-WebRequest "https://api.github.com/repos/FilenCloudDienste/filen-desktop/releases/latest" | ConvertFrom-Json)[0].name
+$release = (Invoke-WebRequest "https://api.github.com/repos/FilenCloudDienste/filen-desktop/releases/latest" | ConvertFrom-Json)[0].body
+
+# write new version and release
+$file = "./filen/filen.nuspec"
+$xml = New-Object XML
+$xml.Load($file)
+$xml.package.metadata.version = $tag
+$xml.package.metadata.releaseNotes = $release
+$xml.Save($file)
+
+# download installer and LICENSE
+Invoke-WebRequest -Uri "https://cdn.filen.io/desktop/release/filen_x64.exe" -OutFile "./filen/tools/filen.exe"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/FilenCloudDienste/filen-desktop/master/LICENSE.md" -OutFile "./filen/legal/LICENSE.txt"
+
+# calculation of checksum
+$TABLE = Get-FileHash "./filen/tools/filen.exe" -Algorithm SHA256
+$SHA = $TABLE.Hash
+
+# writing of VERIFICATION.txt
+$content = "VERIFICATION
+Verification is intended to assist the Chocolatey moderators and community
+in verifying that this package's contents are trustworthy.
+
+The installer have been downloaded from their official github repository listed on <https://github.com/FilenCloudDienste/filen-desktop/releases>
+and can be verified like this:
+
+1. Download the following installer:
+  Version $tag : <https://cdn.filen.io/desktop/release/filen_x64.exe>
+2. You can use one of the following methods to obtain the checksum
+  - Use powershell function 'Get-Filehash'
+  - Use chocolatey utility 'checksum.exe'
+
+  checksum type: SHA256
+  checksum: $SHA
+
+File 'LICENSE.txt' is obtained from <https://raw.githubusercontent.com/FilenCloudDienste/filen-desktop/master/LICENSE.md> " | out-file -filepath ./filen/legal/VERIFICATION.txt
+
+# packaging
+choco pack ./filen/filen.nuspec --outputdirectory .\filen
+
+If ($LastExitCode -eq 0) {
+	choco push ./filen/filen.$tag.nupkg --source https://push.chocolatey.org/
+} else {
+	echo "Error in introduction - Exit code: $LastExitCode "
+}
+
+If ($LastExitCode -eq 0) {
+
+# git and create tag
+git config --local user.email "a-d-r-i@outlook.fr"
+git config --local user.name "A-d-r-i"
+git add .
+git commit -m "[Bot] Update files - filen" --allow-empty
+git tag -a filen-v$tag -m "Filen - version $tag"
+git push -f && git push --tags
+
+# create release
+Install-Module -Name New-GitHubRelease -Force
+Import-Module -Name New-GitHubRelease
+$newGitHubReleaseParameters = @{
+GitHubUsername = "A-d-r-i"
+GitHubRepositoryName = "update_choco_package"
+GitHubAccessToken = "$env:ACTIONS_TOKEN"
+ReleaseName = "Filen v$tag"
+TagName = "filen-v$tag"
+ReleaseNotes = "$release"
+AssetFilePaths = ".\filen\filen.$tag.nupkg"
+IsPreRelease = $false
+IsDraft = $false
+}
+$resultrelease = New-GitHubRelease @newGitHubReleaseParameters
+
+# post tweet
+$twitter = (Select-String -Path config.txt -Pattern "twitter=(.*)").Matches.Groups[1].Value
+if ( $twitter -eq "y" )
+{
+Install-Module PSTwitterAPI -Force
+Import-Module PSTwitterAPI
+$OAuthSettings = @{
+ApiKey = "$env:PST_KEY"
+ApiSecret = "$env:PST_SECRET"
+AccessToken = "$env:PST_TOKEN"
+AccessTokenSecret = "$env:PST_TOKEN_SECRET"
+}
+Set-TwitterOAuthSettings @OAuthSettings
+Send-TwitterStatuses_Update -status "Filen v$tag push now on @chocolateynuget! 
+
+Link: https://community.chocolatey.org/packages/filen/$tag
+@filen_io
+#filen #cloud #release #opensource
+"
+}
+
+# send telegram notification
+Function Send-Telegram {
+Param([Parameter(Mandatory=$true)][String]$Message)
+$Telegramtoken = "$env:TELEGRAM"
+$Telegramchatid = "$env:CHAT_ID"
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$Response = Invoke-RestMethod -Uri "https://api.telegram.org/bot$($Telegramtoken)/sendMessage?chat_id=$($Telegramchatid)&text=$($Message)"}
+
+Send-Telegram -Message "[UCP] New update of Filen : $tag"
+
+} else {
+	echo "Error in choco push - Exit code: $LastExitCode "
+}
