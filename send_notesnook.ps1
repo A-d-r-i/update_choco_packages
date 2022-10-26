3a14982d903c6643bc082b515d45b9b8d8082300
A-d-r-i <27277698+A-d-r-i@users.noreply.github.com>
2022-10-26 22:30:44 +0200

    Update send_notesnook.ps1

--- send_notesnook.ps1
+++ send_notesnook.ps1
@@ -1,6 +1,7 @@
 # extract latest version and release
 $tag = (Invoke-WebRequest "https://api.github.com/repos/streetwriters/notesnook/releases/latest" | ConvertFrom-Json)[0].tag_name
-$tag = $tag -replace 'v'
+#$tag = $tag -replace 'v'
+tag = '2.2.3'
 $release = (Invoke-WebRequest "https://api.github.com/repos/streetwriters/notesnook/releases/latest" | ConvertFrom-Json)[0].body
 
 # write new version and release
