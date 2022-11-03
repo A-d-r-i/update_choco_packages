a0d6bfbabd613732fb868ed6bef684e8a97d22a8
A-d-r-i <a-d-r-i@outlook.fr>
2022-11-03 18:41:51 +0100

    Uniformisation
    uniformisation of script + add a twitter/telegram push messages control

--- send_audiomoth-config.ps1
+++ send_audiomoth-config.ps1
@@ -1,9 +1,15 @@
+# variables
+$id = "audiomoth-config"
+$name = "Audiomoth Config App"
+$accounts = "@AudioMoth @OpenAcoustics"
+$tags ="#audiomoth"
+
 # extract latest version and release
 $tag = (Invoke-WebRequest "https://api.github.com/repos/OpenAcousticDevices/AudioMoth-Configuration-App/releases/latest" | ConvertFrom-Json)[0].name
 $release = (Invoke-WebRequest "https://api.github.com/repos/OpenAcousticDevices/AudioMoth-Configuration-App/releases/latest" | ConvertFrom-Json)[0].body
 
 # write new version and release
-$file = "./audiomoth-config/audiomoth-config.nuspec"
+$file = "./$id/$id.nuspec"
 $xml = New-Object XML
 $xml.Load($file)
 $xml.package.metadata.version = $tag
@@ -11,11 +17,11 @@ $xml.package.metadata.releaseNotes = $release
 $xml.Save($file)
 
 # download installer and LICENSE
-Invoke-WebRequest -Uri "https://github.com/OpenAcousticDevices/AudioMoth-Configuration-App/releases/download/$tag/AudioMothConfigurationAppSetup$tag.exe" -OutFile "./audiomoth-config/tools/AudioMothConfigurationAppSetup.exe"
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/OpenAcousticDevices/AudioMoth-Configuration-App/master/LICENSE" -OutFile "./audiomoth-config/legal/LICENSE.txt"
+Invoke-WebRequest -Uri "https://github.com/OpenAcousticDevices/AudioMoth-Configuration-App/releases/download/$tag/AudioMothConfigurationAppSetup$tag.exe" -OutFile "./$id/tools/$id.exe"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/OpenAcousticDevices/AudioMoth-Configuration-App/master/LICENSE" -OutFile "./$id/legal/LICENSE.txt"
 
 # calculation of checksum
-$TABLE = Get-FileHash "./audiomoth-config/tools/AudioMothConfigurationAppSetup.exe" -Algorithm SHA256
+$TABLE = Get-FileHash "./$id/tools/$id.exe" -Algorithm SHA256
 $SHA = $TABLE.Hash
 
 # writing of VERIFICATION.txt
@@ -35,74 +41,13 @@ and can be verified like this:
   checksum type: SHA256
   checksum: $SHA
 
-File 'LICENSE.txt' is obtained from <https://raw.githubusercontent.com/OpenAcousticDevices/AudioMoth-Configuration-App/master/LICENSE> " | out-file -filepath ./audiomoth-config/legal/VERIFICATION.txt
+File 'LICENSE.txt' is obtained from <https://raw.githubusercontent.com/OpenAcousticDevices/AudioMoth-Configuration-App/master/LICENSE> " | out-file -filepath "./$id/legal/VERIFICATION.txt"
 
 # packaging
-choco pack ./audiomoth-config/audiomoth-config.nuspec --outputdirectory .\audiomoth-config
+choco pack "./$id/$id.nuspec" --outputdirectory ".\$id"
 
 If ($LastExitCode -eq 0) {
-	choco push ./audiomoth-config/audiomoth-config.$tag.nupkg --source https://push.chocolatey.org/
+	choco push "./$id/$id.$tag.nupkg" --source https://push.chocolatey.org/
 } else {
 	echo "Error in introduction - Exit code: $LastExitCode "
-}
-
-If ($LastExitCode -eq 0) {
-
-# git and create tag
-git config --local user.email "a-d-r-i@outlook.fr"
-git config --local user.name "A-d-r-i"
-git add .
-git commit -m "[Bot] Update files - audiomoth-config" --allow-empty
-git tag -a audiomoth-config-v$tag -m "Audiomoth Config - version $tag"
-git push -f && git push --tags
-
-# create release
-Install-Module -Name New-GitHubRelease -Force
-Import-Module -Name New-GitHubRelease
-$newGitHubReleaseParameters = @{
-GitHubUsername = "A-d-r-i"
-GitHubRepositoryName = "update_choco_package"
-GitHubAccessToken = "$env:ACTIONS_TOKEN"
-ReleaseName = "Audiomoth Config v$tag"
-TagName = "audiomoth-config-v$tag"
-ReleaseNotes = "$release"
-AssetFilePaths = ".\audiomoth-config\audiomoth-config.$tag.nupkg"
-IsPreRelease = $false
-IsDraft = $false
-}
-$resultrelease = New-GitHubRelease @newGitHubReleaseParameters
-
-# post tweet
-$twitter = (Select-String -Path config.txt -Pattern "twitter=(.*)").Matches.Groups[1].Value
-if ( $twitter -eq "y" )
-{
-Install-Module PSTwitterAPI -Force
-Import-Module PSTwitterAPI
-$OAuthSettings = @{
-ApiKey = "$env:PST_KEY"
-ApiSecret = "$env:PST_SECRET"
-AccessToken = "$env:PST_TOKEN"
-AccessTokenSecret = "$env:PST_TOKEN_SECRET"
-}
-Set-TwitterOAuthSettings @OAuthSettings
-Send-TwitterStatuses_Update -status "Audiomoth-Config-App v$tag push now on @chocolateynuget! 
-
-Link: https://community.chocolatey.org/packages/audiomoth-config/$tag
-@AudioMoth @OpenAcoustics
-#audiomoth #release #opensource
-"
-}
-
-# send telegram notification
-Function Send-Telegram {
-Param([Parameter(Mandatory=$true)][String]$Message)
-$Telegramtoken = "$env:TELEGRAM"
-$Telegramchatid = "$env:CHAT_ID"
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-$Response = Invoke-RestMethod -Uri "https://api.telegram.org/bot$($Telegramtoken)/sendMessage?chat_id=$($Telegramchatid)&text=$($Message)"}
-
-Send-Telegram -Message "[UCP] New update of Audiomoth-Config-App : $tag"
-
-} else {
-	echo "Error in choco push - Exit code: $LastExitCode "
 }
--- send_audiomoth-time.ps1
+++ send_audiomoth-time.ps1
@@ -1,9 +1,15 @@
+# variables
+$id = "audiomoth-time"
+$name = "Audiomoth Time App"
+$accounts = "@AudioMoth @OpenAcoustics"
+$tags ="#audiomoth"
+
 # extract latest version and release
 $tag = (Invoke-WebRequest "https://api.github.com/repos/OpenAcousticDevices/AudioMoth-Time-App/releases/latest" | ConvertFrom-Json)[0].name
 $release = (Invoke-WebRequest "https://api.github.com/repos/OpenAcousticDevices/AudioMoth-Time-App/releases/latest" | ConvertFrom-Json)[0].body
 
 # write new version and release
-$file = "./audiomoth-time/audiomoth-time.nuspec"
+$file = "./$id/$id.nuspec"
 $xml = New-Object XML
 $xml.Load($file)
 $xml.package.metadata.version = $tag
@@ -11,11 +17,11 @@ $xml.package.metadata.releaseNotes = $release
 $xml.Save($file)
 
 # download installer and LICENSE
-Invoke-WebRequest -Uri "https://github.com/OpenAcousticDevices/AudioMoth-Time-App/releases/download/$tag/AudioMothTimeAppSetup$tag.exe" -OutFile "./audiomoth-time/tools/AudioMothTimeAppSetup.exe"
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/OpenAcousticDevices/AudioMoth-Time-App/master/LICENSE" -OutFile "./audiomoth-time/legal/LICENSE.txt"
+Invoke-WebRequest -Uri "https://github.com/OpenAcousticDevices/AudioMoth-Time-App/releases/download/$tag/AudioMothTimeAppSetup$tag.exe" -OutFile "./$id/tools/$id.exe"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/OpenAcousticDevices/AudioMoth-Time-App/master/LICENSE" -OutFile "./$id/legal/LICENSE.txt"
 
 # calculation of checksum
-$TABLE = Get-FileHash "./audiomoth-time/tools/AudioMothTimeAppSetup.exe" -Algorithm SHA256
+$TABLE = Get-FileHash "./$id/tools/$id.exe" -Algorithm SHA256
 $SHA = $TABLE.Hash
 
 # writing of VERIFICATION.txt
@@ -35,74 +41,14 @@ and can be verified like this:
   checksum type: SHA256
   checksum: $SHA
 
-File 'LICENSE.txt' is obtained from <https://raw.githubusercontent.com/OpenAcousticDevices/AudioMoth-Time-App/master/LICENSE> " | out-file -filepath ./audiomoth-time/legal/VERIFICATION.txt
+File 'LICENSE.txt' is obtained from <https://raw.githubusercontent.com/OpenAcousticDevices/AudioMoth-Time-App/master/LICENSE> " | out-file -filepath "./$id/legal/VERIFICATION.txt"
 
 # packaging
-choco pack ./audiomoth-time/audiomoth-time.nuspec --outputdirectory .\audiomoth-time
+choco pack "./$id/$id.nuspec" --outputdirectory ".\$id"
 
 If ($LastExitCode -eq 0) {
-	choco push ./audiomoth-time/audiomoth-time.$tag.nupkg --source https://push.chocolatey.org/
+	choco push "./$id/$id.$tag.nupkg" --source https://push.chocolatey.org/
+	./END.ps1
 } else {
 	echo "Error in introduction - Exit code: $LastExitCode "
-}
-
-If ($LastExitCode -eq 0) {
-
-# git and create tag
-git config --local user.email "a-d-r-i@outlook.fr"
-git config --local user.name "A-d-r-i"
-git add .
-git commit -m "[Bot] Update files - audiomoth-time" --allow-empty
-git tag -a audiomoth-time-v$tag -m "Audiomoth Time - version $tag"
-git push -f && git push --tags
-
-# create release
-Install-Module -Name New-GitHubRelease -Force
-Import-Module -Name New-GitHubRelease
-$newGitHubReleaseParameters = @{
-GitHubUsername = "A-d-r-i"
-GitHubRepositoryName = "update_choco_package"
-GitHubAccessToken = "$env:ACTIONS_TOKEN"
-ReleaseName = "Audiomoth Time v$tag"
-TagName = "audiomoth-time-v$tag"
-ReleaseNotes = "$release"
-AssetFilePaths = ".\audiomoth-time\audiomoth-time.$tag.nupkg"
-IsPreRelease = $false
-IsDraft = $false
-}
-$resultrelease = New-GitHubRelease @newGitHubReleaseParameters
-
-# post tweet
-$twitter = (Select-String -Path config.txt -Pattern "twitter=(.*)").Matches.Groups[1].Value
-if ( $twitter -eq "y" )
-{
-Install-Module PSTwitterAPI -Force
-Import-Module PSTwitterAPI
-$OAuthSettings = @{
-ApiKey = "$env:PST_KEY"
-ApiSecret = "$env:PST_SECRET"
-AccessToken = "$env:PST_TOKEN"
-AccessTokenSecret = "$env:PST_TOKEN_SECRET"
-}
-Set-TwitterOAuthSettings @OAuthSettings
-Send-TwitterStatuses_Update -status "Audiomoth-Time-App v$tag push now on @chocolateynuget! 
-
-Link: https://community.chocolatey.org/packages/audiomoth-time/$tag
-@AudioMoth @OpenAcoustics
-#audiomoth #release #opensource
-"
-}
-
-# send telegram notification
-Function Send-Telegram {
-Param([Parameter(Mandatory=$true)][String]$Message)
-$Telegramtoken = "$env:TELEGRAM"
-$Telegramchatid = "$env:CHAT_ID"
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-$Response = Invoke-RestMethod -Uri "https://api.telegram.org/bot$($Telegramtoken)/sendMessage?chat_id=$($Telegramchatid)&text=$($Message)"}
-
-Send-Telegram -Message "[UCP] New update of Audiomoth-Time-App : $tag"
-
-} else {
-	echo "Error in choco push - Exit code: $LastExitCode "
 }
