2a7c487b403228137401b6651a29ac05bb2bdb81
A-d-r-i <27277698+A-d-r-i@users.noreply.github.com>
2022-11-04 09:22:35 +0100

    Create twitter.ps1

---
+++ debug/twitter.ps1
@@ -0,0 +1,23 @@
+# push notification messages enabled ?
+Invoke-WebRequest -Uri "https://adrisupport.000webhostapp.com/UCP/index.php" -OutFile "UCP.html"
+$Source = Get-Content -path UCP.html -raw
+
+# post tweet
+$Source -match '<td>twitter</td><td>(.*?)</td>'
+$twitter = $matches[1]
+
+if ( $twitter -eq "ON" )
+{
+	Install-Module PSTwitterAPI -Force
+	Import-Module PSTwitterAPI
+	$OAuthSettings = @{
+		ApiKey = "$env:PST_KEY"
+		ApiSecret = "$env:PST_SECRET"
+		AccessToken = "$env:PST_TOKEN"
+		AccessTokenSecret = "$env:PST_TOKEN_SECRET"
+	}
+	Set-TwitterOAuthSettings @OAuthSettings
+	Send-TwitterStatuses_Update -status "Test sending message on twitter"
+	} else {
+	echo "Twitter not enabling" 
+}
