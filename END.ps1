ccaa28735e940f531fd39553d2273c2be1b4cfb2
A-d-r-i <a-d-r-i@outlook.fr>
2022-11-04 09:43:09 +0100

    Update END.ps1

--- END.ps1
+++ END.ps1
@@ -1,75 +1,75 @@
 If ($LastExitCode -eq 0) {
-# git and create tag
-git config --local user.email "a-d-r-i@outlook.fr"
-git config --local user.name "A-d-r-i"
-git add .
-git commit -m "[Bot] Update files - $id" --allow-empty
-git tag -a $id-v$tag -m "$name - version $tag"
-git push -f && git push --tags
-
-# create release
-Install-Module -Name New-GitHubRelease -Force
-Import-Module -Name New-GitHubRelease
-$newGitHubReleaseParameters = @{
-GitHubUsername = "A-d-r-i"
-GitHubRepositoryName = "update_choco_package"
-GitHubAccessToken = "$env:ACTIONS_TOKEN"
-ReleaseName = "$name v$tag"
-TagName = "$id-v$tag"
-ReleaseNotes = "$release"
-AssetFilePaths = ".\$id\$id.$tag.nupkg"
-IsPreRelease = $false
-IsDraft = $false
-}
-$resultrelease = New-GitHubRelease @newGitHubReleaseParameters
-
-# push notification messages enabled ?
-Invoke-WebRequest -Uri "https://adrisupport.000webhostapp.com/UCP/index.php" -OutFile "UCP.html"
-$Source = Get-Content -path UCP.html -raw
-
-# post tweet
-$Source -match '<td>twitter</td><td>(.*?)</td>'
-$twitter = $matches[1]
-
-if ( $twitter -eq "ON" )
-{
-Install-Module PSTwitterAPI -Force
-Import-Module PSTwitterAPI
-$OAuthSettings = @{
-ApiKey = "$env:PST_KEY"
-ApiSecret = "$env:PST_SECRET"
-AccessToken = "$env:PST_TOKEN"
-AccessTokenSecret = "$env:PST_TOKEN_SECRET"
-}
-Set-TwitterOAuthSettings @OAuthSettings
-Send-TwitterStatuses_Update -status "$name v$tag push now on @chocolateynuget! 
-
-Link: https://community.chocolatey.org/packages/$id/$tag
-$accounts
-$tags #release #opensource
-"
-} else {
-echo "Twitter not enabling"
-}
-
-# send telegram notification
-$Source -match '<td>telegram</td><td>(.*?)</td>'
-$telegram = $matches[1]
-
-if ( $telegram -eq "ON" )
-{
-Function Send-Telegram {
-Param([Parameter(Mandatory=$true)][String]$Message)
-$Telegramtoken = "$env:TELEGRAM"
-$Telegramchatid = "$env:CHAT_ID"
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-$Response = Invoke-RestMethod -Uri "https://api.telegram.org/bot$($Telegramtoken)/sendMessage?chat_id=$($Telegramchatid)&text=$($Message)"}
-
-Send-Telegram -Message "[UCP] New update of $name : $tag - https://community.chocolatey.org/packages/$id/$tag"
-} else {
-echo "Telegram not enabling"
-}
-
-} else {
+	# git and create tag
+	git config --local user.email "a-d-r-i@outlook.fr"
+	git config --local user.name "A-d-r-i"
+	git add .
+	git commit -m "[Bot] Update files - $id" --allow-empty
+	git tag -a $id-v$tag -m "$name - version $tag"
+	git push -f && git push --tags
+	
+	# create release
+	Install-Module -Name New-GitHubRelease -Force
+	Import-Module -Name New-GitHubRelease
+	$newGitHubReleaseParameters = @{
+		GitHubUsername = "A-d-r-i"
+		GitHubRepositoryName = "update_choco_package"
+		GitHubAccessToken = "$env:ACTIONS_TOKEN"
+		ReleaseName = "$name v$tag"
+		TagName = "$id-v$tag"
+		ReleaseNotes = "$release"
+		AssetFilePaths = ".\$id\$id.$tag.nupkg"
+		IsPreRelease = $false
+		IsDraft = $false
+	}
+	$resultrelease = New-GitHubRelease @newGitHubReleaseParameters
+	
+	# push notification messages enabled ?
+	Invoke-WebRequest -Uri "https://adrisupport.000webhostapp.com/UCP/index.php" -OutFile "UCP.html"
+	$Source = Get-Content -path UCP.html -raw
+	
+	# post tweet
+	$Source -match '<td><b>twitter</b></td><td>(.*?)</td>'
+	$twitter = $matches[1]
+	
+	if ( $twitter -eq "ON" )
+	{
+		Install-Module PSTwitterAPI -Force
+		Import-Module PSTwitterAPI
+		$OAuthSettings = @{
+			ApiKey = "$env:PST_KEY"
+			ApiSecret = "$env:PST_SECRET"
+			AccessToken = "$env:PST_TOKEN"
+			AccessTokenSecret = "$env:PST_TOKEN_SECRET"
+		}
+		Set-TwitterOAuthSettings @OAuthSettings
+		Send-TwitterStatuses_Update -status "$name v$tag push now on @chocolateynuget! 
+		
+		Link: https://community.chocolatey.org/packages/$id/$tag
+		$accounts
+		$tags #release #opensource
+		"
+		} else {
+		echo "Twitter not enabling"
+	}
+	
+	# send telegram notification
+	$Source -match '<td><b>telegram</b></td><td>(.*?)</td>'
+	$telegram = $matches[1]
+	
+	if ( $telegram -eq "ON" )
+	{
+		Function Send-Telegram {
+			Param([Parameter(Mandatory=$true)][String]$Message)
+			$Telegramtoken = "$env:TELEGRAM"
+			$Telegramchatid = "$env:CHAT_ID"
+			[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+		$Response = Invoke-RestMethod -Uri "https://api.telegram.org/bot$($Telegramtoken)/sendMessage?chat_id=$($Telegramchatid)&text=$($Message)"}
+		
+		Send-Telegram -Message "[UCP] New update of $name : $tag - https://community.chocolatey.org/packages/$id/$tag"
+		} else {
+		echo "Telegram not enabling"
+	}
+	
+	} else {
 	echo "Error in choco push - Exit code: $LastExitCode "
 }
