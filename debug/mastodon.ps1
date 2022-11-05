4900f2a10143fb69a5354beaf5fb8b86b5543d86
A-d-r-i <27277698+A-d-r-i@users.noreply.github.com>
2022-11-05 16:08:35 +0100

    Create mastodon.ps1

---
+++ debug/mastodon.ps1
@@ -0,0 +1,21 @@
+# push notification messages enabled ?
+Invoke-WebRequest -Uri "https://adrisupport.000webhostapp.com/UCP/index.php" -OutFile "UCP.html"
+$Source = Get-Content -path UCP.html -raw
+
+# post mastodon toot
+$Source -match '<td><b>mastodon</b></td><td>(.*?)</td>'
+$mastodon = $matches[1]
+
+if ( $mastodon -eq "ON" )
+{
+	$Uri = 'https://mastodon.social/api/v1/statuses'
+	$headers = @{
+		Authorization = "$env:MASTODON"
+	}
+	$form = @{
+		status = '[UCP-debug] Test sending message on mastodon'
+	}
+	Invoke-WebRequest -Uri $Uri -Headers $headers -Method Post -Form $form
+	} else {
+	echo "Mastodon not enabling"
+}
