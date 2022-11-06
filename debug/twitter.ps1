0118c2ca3dcb45bc743c7db2b3ad2f9d0bb83633
A-d-r-i <27277698+A-d-r-i@users.noreply.github.com>
2022-11-06 12:48:02 +0100

    Update twitter.ps1

--- debug/twitter.ps1
+++ debug/twitter.ps1
@@ -17,7 +17,13 @@ if ( $twitter -eq "ON" )
 		AccessTokenSecret = "$env:PST_TOKEN_SECRET"
 	}
 	Set-TwitterOAuthSettings @OAuthSettings
-	Send-TwitterStatuses_Update -status "[UCP-debug] Test sending message on twitter"
+	Send-TwitterStatuses_Update -status "Here we are on Twitter! In order to avoid overloading @Ad_r_i's account, it was decided to create a separate account for update_choco_packages.
+
+Find us on:
+
+@joinmastodon: @update_choco_packages@piaille.fr ( https://piaille.fr/@update_choco_packages )
+
+@github: https://github.com/A-d-r-i/update_choco_packages"
 	} else {
 	echo "Twitter not enabling" 
 }
