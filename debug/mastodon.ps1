1cbdbd70f681aced1dbe68e3c83697547f9ef551
A-d-r-i <27277698+A-d-r-i@users.noreply.github.com>
2022-11-06 12:52:05 +0100

    Update mastodon.ps1

--- debug/mastodon.ps1
+++ debug/mastodon.ps1
@@ -13,7 +13,13 @@ if ( $mastodon -eq "ON" )
 		Authorization = "Bearer $env:MASTODON"
 	}
 	$form = @{
-		status = "[UCP-debug] Test sending message on mastodon"
+		status = "Here we are on Mastodon! In order to avoid overloading @A_d_r_i@piaille.fr's account, it was decided to create a separate account for update_choco_packages.
+
+Find us on:
+
+Github: https://github.com/A-d-r-i/update_choco_packages
+
+Twitter: @up_choco_pack@twitter.com ( https://piaille.fr/@update_choco_packages )"
 	}
 	Invoke-WebRequest -Uri $Uri -Headers $headers -Method Post -Form $form
 	} else {
