eec58cbf47dd35d82874f665607cc3e109b48248
A-d-r-i <a-d-r-i@outlook.fr>
2022-11-06 18:53:05 +0100

    Update mastodon.ps1

--- debug/mastodon.ps1
+++ debug/mastodon.ps1
@@ -8,20 +8,9 @@ $mastodon = $matches[1]
 
 if ( $mastodon -eq "ON" )
 {
-	$Uri = 'https://piaille.fr/api/v1/statuses'
-	$headers = @{
-		Authorization = "Bearer $env:MASTODON"
-	}
-	$form = @{
-		status = "Here we are on Mastodon! In order to avoid overloading @A_d_r_i@piaille.fr's account, it was decided to create a separate account for update_choco_packages.
-
-Find us on:
-
-Github: https://github.com/A-d-r-i/update_choco_packages
-
-Twitter: @up_choco_pack@twitter.com ( https://twitter.com/up_choco_pack )"
-	}
-	Invoke-WebRequest -Uri $Uri -Headers $headers -Method Post -Form $form
+	$mastodonheaders = @{Authorization = "Bearer $env:MASTODON"}
+	$mastodonform = @{status = "[UCP-debug] Test sending message on mastodon"}
+	Invoke-WebRequest -Uri "https://piaille.fr/api/v1/statuses" -Headers $mastodonheaders -Method Post -Form $mastodonform
 	} else {
 	echo "Mastodon not enabling"
 }
