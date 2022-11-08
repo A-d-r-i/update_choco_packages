c6df9ed90becc5b6fd15bd79d727412cddf23770
A-d-r-i <a-d-r-i@outlook.fr>
2022-11-08 19:10:34 +0100

    Add saucedacity

---
+++ saucedacity/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Join-Path $toolsDir 'saucedacity32.exe'
+$fileLocation64 = Join-Path $toolsDir 'saucedacity64.exe'
+
+$packageArgs = @{
+  packageName   = 'saucedacity'
+  unzipLocation = $toolsDir
+  file           = $fileLocation
+  file64         = $fileLocation64
+  fileType      = 'EXE'
+  silentArgs     = '/S'
+  softwareName  = 'saucedacity*'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyInstallPackage @packageArgs
+Remove-Item $toolsDir\*.exe -ea 0 -force
