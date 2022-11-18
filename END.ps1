8cc93c06671308539aa80fa7fab1874103933982
A-d-r-i <27277698+A-d-r-i@users.noreply.github.com>
2022-11-18 19:35:43 +0100

    Update END.ps1

--- END.ps1
+++ END.ps1
@@ -22,20 +22,7 @@ If ($LastExitCode -eq 0) {
 		IsDraft = $false
 	}
 	$resultrelease = New-GitHubRelease @newGitHubReleaseParameters
-	
-	# Provide some feedback
-	if ($result.Succeeded -eq $true)
-	{
-		Write-Output "Release published successfully! View it at $($result.ReleaseUrl)"
-	}
-	elseif ($result.ReleaseCreationSucceeded -eq $false)
-	{
-		Write-Error "The release was not created. Error message is: $($result.ErrorMessage)"
-	}
-	elseif ($result.AllAssetUploadsSucceeded -eq $false)
-	{
-		Write-Error "The release was created, but not all of the assets were uploaded to it. View it at $($result.ReleaseUrl). Error message is: $($result.ErrorMessage)"
-	}
+	$resultrelease.ErrorMessage
 	
 	# push notification messages enabled ?
 	Invoke-WebRequest -Uri "https://adrisupport.000webhostapp.com/UCP/index.php" -OutFile "UCP.html"
