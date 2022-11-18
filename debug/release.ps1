b4433181bc53068162c04d1233b4ecf6e4a29af4
A-d-r-i <a-d-r-i@outlook.fr>
2022-11-18 10:27:20 +0100

    Update release.ps1

--- debug/release.ps1
+++ debug/release.ps1
@@ -17,4 +17,17 @@ Install-Module -Name New-GitHubRelease -Force
 		IsDraft = $false
 	}
 	$resultrelease = New-GitHubRelease @newGitHubReleaseParameters
-	$resultrelease.ErrorMessage
+	
+	# Provide some feedback
+	if ($result.Succeeded -eq $true)
+	{
+		Write-Output "Release published successfully! View it at $($result.ReleaseUrl)"
+	}
+	elseif ($result.ReleaseCreationSucceeded -eq $false)
+	{
+		Write-Error "The release was not created. Error message is: $($result.ErrorMessage)"
+	}
+	elseif ($result.AllAssetUploadsSucceeded -eq $false)
+	{
+		Write-Error "The release was created, but not all of the assets were uploaded to it. View it at $($result.ReleaseUrl). Error message is: $($result.ErrorMessage)"
+	}
