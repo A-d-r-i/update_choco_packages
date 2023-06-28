7dcf178f32a087823c9e4e71221a036b484a1b7b
A-d-r-i <a-d-r-i@outlook.fr>
2023-06-28 11:12:15 +0200

    Add Audiomoth-Live

---
+++ audiomoth-live/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Join-Path $toolsDir 'audiomoth-live.exe'
+
+
+$packageArgs = @{
+  packageName   = 'audiomoth-live'
+  unzipLocation = $toolsDir
+  file           = $fileLocation
+  fileType      = 'EXE'
+  silentArgs     = '/S'
+  softwareName  = 'audiomoth-live*'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyInstallPackage @packageArgs
+Remove-Item $toolsDir\*.exe -ea 0 -force
