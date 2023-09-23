44d3a133e05780aeb17ef2d65eef3743276bb6ac
A-d-r-i <27277698+A-d-r-i@users.noreply.github.com>
2023-09-23 21:44:17 +0200

    Update END.ps1

--- END.ps1
+++ END.ps1
@@ -1,7 +1,7 @@
 If ($LastExitCode -eq 0) {
 	# git and create tag
-	git config user.name github-actions[bot]
-	git config user.email github-actions[bot]@users.noreply.github.com
+	git config user.name 'github-actions[bot]'
+	git config user.email 'github-actions[bot]@users.noreply.github.com'
 	git add .
 	git commit -m "[Bot] Update files - $id"
 	git tag -a $id-v$tag -m "$name - version $tag"
