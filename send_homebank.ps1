93fddd3f2eb99dee858f56b6cd70bf6d7c34d326
A-d-r-i <27277698+A-d-r-i@users.noreply.github.com>
2024-08-07 18:47:12 +0200

    Correction of HomeBank

--- send_homebank.ps1
+++ send_homebank.ps1
@@ -5,8 +5,8 @@ $accounts = ""
 $tags = "#homebank"
 
 # extract latest version and release
-Invoke-WebRequest -Uri "http://homebank.free.fr/en/downloads.php" -OutFile "HOMEBANK.html"
-Invoke-WebRequest -Uri "http://homebank.free.fr/ChangeLog" -OutFile "release.txt"
+Invoke-WebRequest -Uri "https://www.gethomebank.org/en/downloads.php" -OutFile "HOMEBANK.html"
+Invoke-WebRequest -Uri "https://www.gethomebank.org/ChangeLog" -OutFile "release.txt"
 $Source = Get-Content -path HOMEBANK.html -raw
 $text = Get-Content -path release.txt
 $Source -match 'The latest <b>([0-9]+(\.[0-9]+)+) stable</b>'
@@ -22,7 +22,7 @@ $release = $release -replace '   *', "`n"
 $release = $release -replace '  ', "`n"
 $regex = '([0-9]{7,})'
 $release = $release -replace $regex, '[${1}](https://bugs.launchpad.net/bugs/${1})'
-$release = -join($release, "`n**Full changelog:** [http://homebank.free.fr/ChangeLog](http://homebank.free.fr/ChangeLog)");
+$release = -join($release, "`n**Full changelog:** [https://www.gethomebank.org/ChangeLog](https://www.gethomebank.org/ChangeLog)");
 
 # write new version and release
 $file = "./$id/$id.nuspec"
@@ -33,7 +33,7 @@ $xml.package.metadata.releaseNotes = $release
 $xml.Save($file)
 
 # download installer
-Invoke-WebRequest -Uri "http://homebank.free.fr/public/binaries/HomeBank-$tag-setup.exe" -OutFile "./$id/tools/$id.exe"
+Invoke-WebRequest -Uri "https://www.gethomebank.org/public/binaries/HomeBank-$tag-setup.exe" -OutFile "./$id/tools/$id.exe"
 
 Remove-Item release.txt
 
@@ -46,11 +46,11 @@ $content = "VERIFICATION
 Verification is intended to assist the Chocolatey moderators and community
 in verifying that this package's contents are trustworthy.
 
-The installer have been downloaded from their official site repository listed on <http://homebank.free.fr/fr/downloads.php>
+The installer have been downloaded from their official site repository listed on <https://www.gethomebank.org/en/downloads.php>
 and can be verified like this:
 
 1. Download the following installer:
-  Version $tag : <http://homebank.free.fr/public/binaries/HomeBank-$tag-setup.exe>
+  Version $tag : <https://www.gethomebank.org/public/binaries/HomeBank-$tag-setup.exe>
 2. You can use one of the following methods to obtain the checksum
   - Use powershell function 'Get-Filehash'
   - Use chocolatey utility 'checksum.exe'
