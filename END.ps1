2dbea092d818858a0782f77e9b93036225564b67
A-d-r-i <27277698+A-d-r-i@users.noreply.github.com>
2026-09-14 12:54:12 +0200

    Simplification

--- END.ps1
+++ END.ps1
@@ -24,64 +24,6 @@ If ($LastExitCode -eq 0) {
 	$resultrelease = New-GitHubRelease @newGitHubReleaseParameters
 	$resultrelease.ErrorMessage
 	
-	# push notification messages enabled ?
-	Invoke-WebRequest -Uri "http://adrisupport.free.nf/UCP/index.php" -OutFile "UCP.html"
-	$Source = Get-Content -path UCP.html -raw
-	
-	# post twitter tweet
-	$Source -match '<td><b>twitter</b></td><td>(.*?)</td>'
-	$twitter = $matches[1]
-	
-	if ( $twitter -eq "ON" )
-	{
-		Install-Module PSTwitterAPI -Force
-		Import-Module PSTwitterAPI
-		$OAuthSettings = @{
-			ApiKey = "$env:PST_KEY"
-			ApiSecret = "$env:PST_SECRET"
-			AccessToken = "$env:PST_TOKEN"
-			AccessTokenSecret = "$env:PST_TOKEN_SECRET"
-		}
-		Set-TwitterOAuthSettings @OAuthSettings
-		Send-TwitterStatuses_Update -status "$name v$tag push now on @chocolateynuget! 
-		
-		Link: https://community.chocolatey.org/packages/$id/$tag
-		$accounts
-		$tags #release #opensource
-		"
-		} else {
-		echo "Twitter not enabling"
-	}
-	
-	# send telegram notification
-	$Source -match '<td><b>telegram</b></td><td>(.*?)</td>'
-	$telegram = $matches[1]
-	
-	if ( $telegram -eq "ON" )
-	{
-		$tmtext = "[UCP] New update of $name : $tag - https://community.chocolatey.org/packages/$id/$tag"
-		$tmtoken = "$env:TELEGRAM"
-		$tmchatid = "$env:CHAT_ID"
-		Invoke-RestMethod -Uri "https://api.telegram.org/bot$tmtoken/sendMessage?chat_id=$tmchatid&text=$tmtext"
-		} else {
-	echo "Telegram not enabling"}
-	
-	
-	# post mastodon toot
-	$Source -match '<td><b>mastodon</b></td><td>(.*?)</td>'
-	$mastodon = $matches[1]
-	
-	if ( $mastodon -eq "ON" )
-	{
-		$mastodonheaders = @{Authorization = "Bearer $env:MASTODON"}
-		$mastodonform = @{status = "$name v$tag push now on @chocolateynuget@twitter.com! 
-			
-			Link: https://community.chocolatey.org/packages/$id/$tag
-		$tags #release #opensource"}
-		Invoke-WebRequest -Uri "https://piaille.fr/api/v1/statuses" -Headers $mastodonheaders -Method Post -Form $mastodonform
-		} else {
-	echo "Mastodon not enabling"}
-	
 	} else {
 	echo "Error in choco push - Exit code: $LastExitCode "
 }
